b2de9330ed1f8adda33f65329408d1ce60618794
Marko Sabotin <marko.sabotin@telekom.si>
2015-09-24 07:44:37 +0200

    no message

--- TBPreview/SabilandTBPreview/SabilandTBPreview/GameViewController.swift
+++ TBPreview/SabilandTBPreview/SabilandTBPreview/GameViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         if let scene = GameScene(fileNamed:"GameScene") {
             // Configure the view.
             let skView = self.view as! SKView
@@ -29,24 +29,20 @@ class GameViewController: UIViewController {
             skView.presentScene(scene)
         }
     }
-
+    
     override func shouldAutorotate() -> Bool {
         return true
     }
-
+    
     override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return .AllButUpsideDown
-        } else {
-            return .All
-        }
+        return UIInterfaceOrientationMask.Portrait
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
+    
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
