a71a402af524fd15e7cb1f2625dedc1327c0b586
Marko Sabotin <marko.sabotin@telekom.si>
2015-09-24 07:57:30 +0200

    no message

--- TBPreview/SabilandTBPreview/SabilandTBPreview/GameScene.swift
+++ TBPreview/SabilandTBPreview/SabilandTBPreview/GameScene.swift
@@ -17,29 +17,21 @@ class GameScene: SKScene {
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:10.0);
         myLabel.zPosition = 1000.0
         self.addChild(myLabel)
+        setNewSabilandTrippyBackground()
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+        setNewSabilandTrippyBackground()
     }
     
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
+    
+    private func setNewSabilandTrippyBackground()
+    {
+        
+    }
+    
 }
